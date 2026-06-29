63a6514f13c02b52bf2fb17258588bf620194266
SARATHPR13 <sarathpr13@gmail.com>
2026-06-29 14:35:04 +0530

    Fix: Remove KEY_DYNAMIC_COLOR from MainActivity

--- app/src/main/java/com/lumio/app/MainActivity.kt
+++ app/src/main/java/com/lumio/app/MainActivity.kt
@@ -7,32 +7,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.lumio.app.data.preferences.AppPreferences
 import com.lumio.app.presentation.navigation.LumioNavGraph
 import com.lumio.app.presentation.theme.LumioTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var appPreferences: AppPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContent {
-            val dynamicColor by appPreferences.KEY_DYNAMIC_COLOR.collectAsState(initial = true)
-
-            LumioTheme(
-                dynamicColor = dynamicColor
-            ) {
+            LumioTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color    = MaterialTheme.colorScheme.background
